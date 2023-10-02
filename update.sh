6236f1fadec2be4ae71fd6764e46edc4e74c942c
johannes hanika <hanatos@gmail.com>
2023-10-02 10:10:17 +0200

    add update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VKDTDIR=../vkdt
+cd ${VKDTDIR}
+VER=$(make release | cut -f4 -d' ')
+cd -
+git rm -rf bin/ doc/ ext/ src/
+tar xJf ${VKDTDIR}/vkdt-${VER}.tar.xz --strip-components=1
+git add $(tar tJf ${VKDTDIR}/vkdt-${VER}.tar.xz | cut -f2- -d/)
+git commit -a -m "update to ${VER}"
